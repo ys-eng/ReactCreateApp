50d55ecb870f3e1cb86d44333f640b6f90777c76
yamamoto <shanbenjunjie@gmail.com>
2021-02-27 19:39:10 +0900

    ClockComponent.tsxを修正

--- src/components/ClockComponent.tsx
+++ src/components/ClockComponent.tsx
@@ -5,13 +5,13 @@ import "./ClockComponent.css";
 const ClockComponent = () => {
 
   let hourHandState = {
-    transform: "rotate(0)",
+    transform: `rotate(0deg)`,
   };
   let minuteHandState = {
-    transform: "rotate(0)",
+    transform: `rotate(0deg)`,
   };
   let secondHandState = {
-    transform: "rotate(0)",
+    transform: `rotate(0deg)`,
   };
   setInterval(() => {
     const now = moment();
@@ -19,7 +19,7 @@ const ClockComponent = () => {
     const minute: number = now.minutes();
     const second: number = now.seconds();
     const millisecond: number = now.milliseconds();
-    
+
     // 時針は1時間で360/12度、1分間で360/12/60度、1秒間で360/12/60/60度動く
     const deltaMinute = minute === 0 ? 0 : (minute * 360) / 12 / 60;
     hourHandState = {
@@ -29,13 +29,13 @@ const ClockComponent = () => {
     // 分針は1時間で360度、1分間で360/60度、1秒間で360/60/60度動く
     const deltaSecond = second === 0 ? 0 : (second * 360) / 60 / 60;
     minuteHandState = { 
-      transform: `rotate(${hour * 30 + deltaSecond}deg)`,
+      transform: `rotate(${minute * 6 + deltaSecond}deg)`,
     };
 
     // 秒針は1分間で360度、1秒間で360/60度動く、1ミリ秒で360/60/1000度動く
     const deltaMillisecond = millisecond === 0 ? 0 : (millisecond * 360) / 60 / 1000;
     secondHandState = { 
-      transform: `rotate(${hour * 30 + deltaMillisecond}deg)`,
+      transform: `rotate(${second * 6 + deltaMillisecond}deg)`,
     };
 
   }, 10);
