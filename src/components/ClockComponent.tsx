86735f432c18b727b633e5732f1859c2781e8704
yamamoto <shanbenjunjie@gmail.com>
2021-03-01 06:15:46 +0900

    針の動き、デザインを編集

--- src/components/ClockComponent.tsx
+++ src/components/ClockComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import moment, { Moment } from "moment";
 import "./ClockComponent.css";
 
@@ -14,13 +14,11 @@ const ClockComponent = () => {
     const HourHandState = {
       transform: `rotate(${hour * 30 + deltaMinute}deg)`,
     };
-
     // 分針は1時間で360度、1分間で360/60度、1秒間で360/60/60度動く
     const deltaSecond = second === 0 ? 0 : (second * 360) / 60 / 60;
     const MinuteHandState = { 
       transform: `rotate(${minute * 6 + deltaSecond}deg)`,
     };
-
     // 秒針は1分間で360度、1秒間で360/60度動く、1ミリ秒で360/60/1000度動く
     const deltaMillisecond = millisecond === 0 ? 0 : (millisecond * 360) / 60 / 1000;
     const SecondHandState = { 
