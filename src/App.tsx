656e9d6057ccfb0e5d55e67956c85dc402ac2eda
yamamoto <shanbenjunjie@gmail.com>
2021-03-01 10:36:39 +0900

    absoluteをtailwindで記述

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <div className="reactClock">
+        <div className="reactClock absolute">
           <ClockComponent></ClockComponent>
         </div>
     </div>
