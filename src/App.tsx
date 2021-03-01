a040fa37cb7993c8ff1a348f2a5b76357b41edca
yamamoto <shanbenjunjie@gmail.com>
2021-03-01 10:28:55 +0900

    ゼニガメボタンの消去

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ClockComponent from "./components/ClockComponent";
-import ZenigameButton from "./components/ZenigameButton";
 
 function App() {
   return (
