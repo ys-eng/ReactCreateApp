8154d24818fba4611b89732c20b6b83a596d102c
yamamoto <shanbenjunjie@gmail.com>
2021-03-10 17:01:36 +0900

    プラスボタンの実装

--- src/components/CalculatorComponent.tsx
+++ src/components/CalculatorComponent.tsx
@@ -4,138 +4,36 @@ import CalcBtnComponent from "./CalcBtnComponent";
 
 let displayArray: string[] = [];
 let calcArray: string[] = [];
+
 const CalculatorComponent = () => {
-  // const btns: string[] = [
-  //   "0",
-  //   "1",
-  //   "2",
-  //   "3",
-  //   "4",
-  //   "5",
-  //   "6",
-  //   "7",
-  //   "8",
-  //   "9",
-  //   "dot",
-  // ];
-  // const operatorArray: string[] = ["plus", "minus", "multiply", "divide"];
-
-  // let calcArray: string[] = [];
-  // let totalArray: string[] = [];
-  // let check: string = "";
-  // enum CalcStatus {
-  //   empty = "",
-  //   calcEnd = "calcEnd",
-  //   error = "error",
-  //   plus = "plus",
-  //   minus = "minus",
-  //   multiply = "multiply",
-  //   divide = "divide",
-  // }
-
-  // for (const elementId of btns) {
-  //   let value: string = elementId;
-  //   if (value === "dot") {
-  //     value = ".";
-  //   }
-  //   const btnElement: HTMLElement = <HTMLElement>(
-  //     document.getElementById(`button-${elementId}`)
-  //   );
-  //   btnElement.onclick = () => {
-  //     if (check === CalcStatus.calcEnd || check === CalcStatus.error) {
-  //       check = CalcStatus.error;
-  //       document.getElementById("button-result").innerHTML = check;
-  //     } else {
-  //       calcArray.push(value);
-  //       document.getElementById("button-result").innerHTML = calcArray.join("");
-  //     }
-  //   };
-  // }
-
-  // // 演算子ボタン
-  // for (const operator of operatorArray) {
-  //   const btnOperator: HTMLElement = <HTMLElement>(
-  //     document.getElementById(`button-${operator}`)
-  //   );
-  //   btnOperator.onclick = () => {
-  //     if (check === CalcStatus.error) {
-  //       document.getElementById("button-result").innerHTML = check;
-  //     } else if (check === CalcStatus.calcEnd) {
-  //       check = operator;
-  //       calcArray = [];
-  //     } else {
-  //       check = operator;
-  //       totalArray.push(calcArray.join(""));
-  //       calcArray = [];
-  //     }
-  //   };
-  // }
-  // // イコールボタン
-  // let total: string = "";
-  // const btnEq: HTMLElement = <HTMLElement>document.getElementById(`button-eq`);
-  // btnEq.onclick = () => {
-  //   totalArray.push(calcArray.join(""));
-  //   calcArray = [];
-  //   const result: string = sum(totalArray, check);
-  //   document.getElementById("button-result").innerHTML = result;
-  //   totalArray = [];
-  //   totalArray.push(result);
-  //   check = CalcStatus.calcEnd;
-  // };
-
-  // const sum = (totalArray: string[], calcStatus: string): string => {
-  //   let func;
-  //   if (calcStatus === CalcStatus.plus) {
-  //     func = (sum, val) => sum + val;
-  //   } else if (calcStatus === CalcStatus.minus) {
-  //     func = (sum, val) => sum - val;
-  //   } else if (calcStatus === CalcStatus.multiply) {
-  //     func = (sum, val) => sum * val;
-  //   } else if (calcStatus === CalcStatus.multiply) {
-  //     func = (sum, val) => sum * val;
-  //   }
-  //   return String(totalArray.map(Number).reduce(func));
-  // };
-
-  // // クリアボタン
-  // const btnclear: HTMLElement = <HTMLElement>(
-  //   document.getElementById(`button-clear`)
-  // );
-  // btnclear.onclick = () => {
-  //   document.getElementById("button-result").innerHTML = "0";
-  //   calcArray = [];
-  //   totalArray = [];
-  //   check = CalcStatus.empty;
-  // };
   const grayColor = "bg-gray-600 hover:bg-gray-400 text-white";
   const whiteColor = "bg-gray-300 hover:bg-gray-100 text-black";
   const orangeColor = "bg-yellow-500 hover:bg-yellow-300 text-white";
 
   const [display, setDisplay] = useState("0");
-  const clickCalcBtn = (title: string) => {
+  // ナンバーボタン+ドットボタン
+  const numberBtn = (title: string) => {
     displayArray.push(title);
-    setDisplay(displayArray.join(""));
-    if(title === "＋") {
-      title = "+";
-    } else if(title === "−") {
-      title = "-";
-    } else if(title === "×") {
-      title = "*";
-    } else if(title === "÷") {
-      title = "/";
-    };
     calcArray.push(title)
+    setDisplay(displayArray.join(""));
   };
+  // イコールボタン
   const eqBtn = () => {
     const result = Function('return ('+calcArray.join("")+');')();
     displayArray = [result];
     calcArray = [result];
     setDisplay(displayArray.join(""));
   };
+  // クリアボタン
   const clearBtn = () => {
     displayArray = [];
     calcArray = []; 
     setDisplay("0");
+  }
+  // プラスボタン
+  const plusBtn = () => {
+    displayArray = [];
+    calcArray.push("+");
   }
 
   return (
@@ -155,87 +53,87 @@ const CalculatorComponent = () => {
         <CalcBtnComponent
           title="+/-"
           className={whiteColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="%"
           className={whiteColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="÷"
           className={orangeColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="7"
           className={grayColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="8"
           className={grayColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="9"
           className={grayColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="×"
           className={orangeColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="4"
           className={grayColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="5"
           className={grayColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="6"
           className={grayColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="−"
           className={orangeColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="1"
           className={grayColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="2"
           className={grayColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="3"
           className={grayColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="＋"
           className={orangeColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={plusBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="0"
           className={`col-span-2 ${grayColor}`}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="."
           className={grayColor}
-          onClickCalcBtn={clickCalcBtn}
+          onClickCalcBtn={numberBtn}
         ></CalcBtnComponent>
         <CalcBtnComponent
           title="="
